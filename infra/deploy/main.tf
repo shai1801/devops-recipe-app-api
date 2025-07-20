a26d1c3f2d8267cc47b74363980a3b84c7ed1130
shai1801 <shai1801@gmail.com>
2025-07-20 22:24:57 +0100

    Configured the terraform deploy file.

---
+++ infra/deploy/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "devops-recipe-app-tf-state-shai"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "eu-north-1"
+    encrypt              = true
+    dynamodb_table       = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManagedBy   = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+data "aws_region" "current" {
+
+}
