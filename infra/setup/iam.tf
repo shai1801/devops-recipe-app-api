0760b698c21a154e984de32c0e2b967646d6793e
shai1801 <shai1801@gmail.com>
2025-07-23 17:48:06 +0100

    ECR configuration updated.

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -3,7 +3,7 @@
 ####################################################################
 
 resource "aws_iam_user" "cd" {
-    name = "recipe-app-api-cd"
+  name = "recipe-app-api-cd"
 }
 
 resource "aws_iam_access_key" "cd" {
@@ -16,13 +16,13 @@ resource "aws_iam_access_key" "cd" {
 
 data "aws_iam_policy_document" "tf_backend" {
   statement {
-    effect = "Allow"
-    actions = ["s3:ListBucket"]
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
 
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
@@ -43,12 +43,50 @@ data "aws_iam_policy_document" "tf_backend" {
 }
 
 resource "aws_iam_policy" "tf_backend" {
-  name = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
   description = "Allow user to use S3 and DynamoDB for TF backend resources"
-  policy = data.aws_iam_policy_document.tf_backend.json
+  policy      = data.aws_iam_policy_document.tf_backend.json
 }
 
 resource "aws_iam_user_policy_attachment" "tf_backend" {
-  user = aws_iam_user.cd.name
+  user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
+}
+
+#########################
+# Policy for ECR access #
+#########################
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
 }
--- infra/setup/output.tf
+++ infra/setup/output.tf
@@ -8,3 +8,9 @@ output "cd_user_access_key_secret" {
   value       = aws_iam_access_key.cd.secret
   sensitive   = true
 }
+
+
+output "ecr_repo_app" {
+  description = "ECR Repository URL for app image"
+  value = aws_ecr_repository.proxy.repository_url
+}
