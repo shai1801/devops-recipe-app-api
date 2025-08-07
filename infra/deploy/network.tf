1ca9de6ba2b855246c3f0ad73e5a2b226908f79d
shai1801 <shai1801@gmail.com>
2025-08-07 23:06:03 +0100

    subnet configuration added

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.1..1.0/24"
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_region.current.name}a"
 
@@ -53,7 +53,7 @@ resource "aws_route" "public_internet_access_a" {
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.1..2.0/24"
+  cidr_block              = "10.1.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_region.current.name}b"
 
