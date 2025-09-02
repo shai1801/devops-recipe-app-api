db8f8a1fc713689548539cacff8fa2e958e78041
shai1801 <shai1801@gmail.com>
2025-09-02 22:39:13 +0100

    RDS instance

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -16,8 +16,10 @@ resource "aws_iam_access_key" "cd" {
 
 data "aws_iam_policy_document" "tf_backend" {
   statement {
-    effect    = "Allow"
-    actions   = ["s3:ListBucket"]
+    effect = "Allow"
+    actions = ["s3:ListBucket", "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
 
